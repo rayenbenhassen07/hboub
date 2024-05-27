bc797e289bf4b2b3ef18206a98d7f630a6306b71
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-05-27 23:43:08 +0100

    pagesssssssssssssssss

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,51 @@ import getListings, {
 } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
-import ListingPage from "./components/listings/ListingPage";
 
+export const dynamic = "force-dynamic";
 
 interface HomeProps {
   searchParams: IListingsParams
 };
 
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
 
-const Home = async ( { searchParams }: HomeProps ) => {
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
 
   return (
-    <ListingPage searchParams={searchParams} />
+    <ClientOnly>
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
 
