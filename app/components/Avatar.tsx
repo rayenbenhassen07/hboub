a0814e8680533dd2f2c81d57dc27aca6c0cda415
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-05-29 02:01:33 +0100

    azazs

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,16 +4,21 @@ import Image from "next/image";
 
 interface AvatrProps{
     src?: string | null | undefined;
+    width?: number;
+    height?: number;
 }
 
 const Avatar : React.FC<AvatrProps> = ({
-    src
+    src,
+    width,
+    height
+
 }) => {
   return (
     <Image
         className="rounded-full"
-        height={30}
-        width={30}
+        height={width}
+        width={height}
         alt="Avatar"
         src={src || "/images/avatar.png"}
     
